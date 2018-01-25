ad32fac91ae7c12037685c049c8c6469170fd964
Erik Paulo Lacerda <erik.lacera@gmail.com>
2018-01-25 15:35:21 -0200

    [Bug]

--- src/main/java/com/softb/savefy/account/service/InvestmentAccountService.java
+++ src/main/java/com/softb/savefy/account/service/InvestmentAccountService.java
@@ -8,7 +8,6 @@ import com.softb.savefy.account.repository.AccountEntryRepository;
 import com.softb.savefy.account.repository.AccountRepository;
 import com.softb.savefy.account.repository.QuoteSaleRepository;
 import com.softb.savefy.account.web.resource.AssetPrice;
-import com.softb.savefy.utils.AppDate;
 import com.softb.savefy.utils.AppMaths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +15,6 @@ import org.springframework.stereotype.Service;
 import javax.inject.Inject;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -151,6 +149,8 @@ public class InvestmentAccountService extends AbstractAccountService {
             entry = saveIRLawEntry(account, entry, groupId);
         }
 
+        updateLastPrice(new AssetPrice(account.getId(), entry.getDate(), entry.getQuoteLastValue()), groupId);
+
         return entry;
     }
 
@@ -179,6 +179,7 @@ public class InvestmentAccountService extends AbstractAccountService {
 
     private InvestmentAccountEntry savePurchaseEntry(InvestmentAccount account, InvestmentAccountEntry entry, Integer groupId){
         InvestmentAccountEntry savedEntry = (InvestmentAccountEntry) save(entry, groupId);
+
         return savedEntry;
     }
 
@@ -260,79 +261,10 @@ public class InvestmentAccountService extends AbstractAccountService {
 
     private IncomeTaxRateService.TaxRange getTaxRange(InvestmentAccount account, InvestmentAccountEntry entry){
         return incomeTaxRateService.getTaxRange(account, entry);
-//        TaxRange range = new TaxRange(0.0, new Date());
-//
-//        Long age = getAge(entry);
-//        Calendar cal = Calendar.getInstance();
-//        cal.setTime(entry.getDate());
-//
-//        Double taxRange = 0.0;
-//        if (account.getProduct().equals(InvestmentAccount.Product.FIXED_INCOME) || account.getProduct().equals(InvestmentAccount.Product.MULTI_SHARES)){
-//            if (age>=0 && age<=180) {
-//                range.taxRange = 0.225;
-//                cal.add(Calendar.DAY_OF_YEAR,180);
-//            } else if (age>=181 && age<=360) {
-//                range.taxRange = 0.200;
-//                cal.add(Calendar.DAY_OF_YEAR,360);
-//            } else if (age>=361 && age<=720) {
-//                range.taxRange = 0.175;
-//                cal.add(Calendar.DAY_OF_YEAR,720);
-//            } else if (age>720) {
-//                range.taxRange = 0.150;
-//                cal = null;
-//            }
-//        } else if (account.getProduct().equals(InvestmentAccount.Product.PENSION_FUND)){
-//            if (age>=0 && age<=720) {
-//                range.taxRange = 0.350;
-//                cal.add(Calendar.DAY_OF_YEAR,720);
-//            } else if (age>=721 && age<=1440) {
-//                range. taxRange = 0.300;
-//                cal.add(Calendar.DAY_OF_YEAR,1440);
-//            } else if (age>=1441 && age<=2160) {
-//                range.taxRange = 0.250;
-//                cal.add(Calendar.DAY_OF_YEAR,2160);
-//            } else if (age>=2161 && age<=2880) {
-//                range.taxRange = 0.200;
-//                cal.add(Calendar.DAY_OF_YEAR,2880);
-//            } else if (age>=2881 && age<=3600) {
-//                range.taxRange = 0.150;
-//                cal.add(Calendar.DAY_OF_YEAR,3600);
-//            } else if (age>3600) {
-//                range.taxRange = 0.100;
-//                cal = null;
-//            }
-//        } else if (account.getProduct().equals(InvestmentAccount.Product.FUND_OF_SHARES)){
-//            range.taxRange = 0.150;
-//            cal = null;
-//        }
-//        range.nextRangeDate = (cal != null ? cal.getTime() : null);
-//
-//        return range;
     }
 
     private Double getMaxTaxRange(InvestmentAccount account){
         return incomeTaxRateService.getMaxTaxRange(account);
-//        Double taxRange = 0.0;
-//        if (account.getProduct().equals(InvestmentAccount.Product.FIXED_INCOME) || account.getProduct().equals(InvestmentAccount.Product.MULTI_SHARES)){
-//            taxRange = 0.150;
-//        } else if (account.getProduct().equals(InvestmentAccount.Product.PENSION_FUND)){
-//            taxRange = 0.100;
-//        } else if (account.getProduct().equals(InvestmentAccount.Product.FUND_OF_SHARES)){
-//            taxRange = 0.150;
-//        }
-//
-//        return taxRange;
 
     }
-
-    private Long getAge(InvestmentAccountEntry entry) {
-        Date curDate = new Date();
-        return AppDate.getDifferenceDays(entry.getDate(), curDate);
-    }
-
-//    @AllArgsConstructor
-//    private class TaxRange{
-//        Double taxRange;
-//        Date nextRangeDate;
-//    }
 }
--- src/main/java/com/softb/savefy/account/web/resource/AssetPrice.java
+++ src/main/java/com/softb/savefy/account/web/resource/AssetPrice.java
@@ -1,5 +1,6 @@
 package com.softb.savefy.account.web.resource;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.Date;
  */
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class AssetPrice implements Serializable {
 
 	private static final long serialVersionUID = 1L;
